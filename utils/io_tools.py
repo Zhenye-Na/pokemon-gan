96c46adee47e7e2fa66e33c03961b5ae95661b46
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-03-20 04:16:29 -0500

    Update io_tools.py

--- utils/io_tools.py
+++ utils/io_tools.py
@@ -5,7 +5,7 @@ import skimage
 import os
 from skimage import io
 
-def read_dataset(image_data_path):
+def read_dataset(data_txt_file, image_data_path):
     """Read data into a Python dictionary.
 
     Args:
@@ -23,26 +23,18 @@ def read_dataset(image_data_path):
             N is the number of examples in the data split, the examples should
             be stored in the same order as in the txt file.
     """
+    # train_set = read_dataset("../data/train.txt", "../data/image_data/")
     data = {}
-    data['image'] = []
-    data['label'] = []
+    # data['image'] = []
+    # data['label'] = []
 
     # Read txt file
     with open(data_txt_file, 'r') as f:
         for line in f:
-            # Split the string to imgdirs & labels
-            dirs = line.rstrip().split(',')
+            # Extract filename for each image
+            imname = line.rstrip()
 
-            # output of dirs -> ['0000', '-1']
-            data['label'].append([int(dirs[1])])
-
-            # Append '.jpg' to filename
-            imname = dirs[0] + '.jpg'
             imgdir = os.path.join(image_data_path, imname)
-            data['image'].append(io.imread(imgdir))
-
-    # Transform to Numpy array with specific dimension
-    data['label'] = np.array(data['label'])
-    data['image'] = np.array(data['image'])
+            data[imname] = io.imread(imgdir)
 
     return data
