026b0f57889b9a92c87bcaef3538a06a24ca14f6
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-03-20 04:18:27 -0500

    Update pipeline

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ import tensorflow as tf
 
 # from models.support_vector_machine import SupportVectorMachine
 # from train_eval_model import train_model, eval_model, train_model_qp
-# from utils.io_tools import read_dataset
+from utils.io_tools import read_dataset
 # from utils.data_tools import preprocess_data
+
+
+
+# Read image into dictionary
+data = train_set = read_dataset("data/train.txt", "data/image_data/")
