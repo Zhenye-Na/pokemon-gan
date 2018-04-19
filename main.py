1cfe5ca904a0dd7d393a00cf12485040207ad2c2
namanuiuc <namanatulshukla@gmail.com>
2018-04-18 19:40:54 -0500

    Updated 800 epochs

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ flags.DEFINE_float('learning_rate', 2e-4, 'Initial learning rate.')
 flags.DEFINE_integer('HEIGHT', 128, 'Height of images.')
 flags.DEFINE_integer('WIDTH', 128, 'Width of images.')
 flags.DEFINE_integer('CHANNEL', 3, 'Number of channels of images.')
-flags.DEFINE_integer('EPOCH', 5000, 'Number of Epochs to run.')
+flags.DEFINE_integer('EPOCH', 1000, 'Number of Epochs to run.')
 flags.DEFINE_integer('BATCH_SIZE', 64, 'Size of batch in update.')
 flags.DEFINE_string('VERSION', 'newPokemon', 'Directory of output')
 flags.DEFINE_string('process_method', 'default',
@@ -49,7 +49,6 @@ def main(_):
     process_method = FLAGS.process_method
     # newPoke_path = './' + VERSION
     newPoke_path = './' + FLAGS.VERSION
-
 
     # Training
     train(
